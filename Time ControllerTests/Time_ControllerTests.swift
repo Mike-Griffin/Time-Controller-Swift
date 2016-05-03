f9450c32decd9efc47e82d76e6eef61374b7e93c
Mike-Griffin <mike_griffin@comcast.net>
2016-05-02 20:18:33 -0700

    Initial Commit

---
+++ Time ControllerTests/Time_ControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Time_ControllerTests.swift
+//  Time ControllerTests
+//
+//  Created by Mike Griffin on 5/2/16.
+//  Copyright © 2016 Mike Griffin. All rights reserved.
+//
+
+import XCTest
+@testable import Time_Controller
+
+class Time_ControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
